507aecf022f7c38f7e4d584ad082dc7b22e46475
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2023-05-10 19:26:35 +0400

    Initial Changes
    Initial Changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
-import VIP from './components/VIP';
-import Guest from './components/Guest';
-import Media from './components/Media';
 
-import Success from './components/Success';
-import Success2 from './components/Success2';
-import Error from './components/Error';
-
-import VIPRSVP from './components/VIPRSVP';
-import GuestRSVP from './components/GuestRSVP';
-import MediaRSVP from './components/MediaRSVP';
+import Rose from './components/Rose';
+import Said from './components/Said';
+import Farangez from './components/Farangez';
 
 function App() {
   return (
@@ -19,19 +12,9 @@ function App() {
       <BrowserRouter>
         <Routes>
 
-            <Route exact path="/" element={<Error />} />
-            <Route exact path="*" element={<Error />} />
-
-            <Route path="/vipregistration" element={<VIP />} />
-            <Route path="/guestregistration" element={<Guest />} />
-            <Route path="/mediaregistration" element={<Media />} />
-
-            <Route path="/viprsvp" element={<VIPRSVP/>} />
-            <Route path="/guestrsvp" element={<GuestRSVP />} />
-            <Route path="/mediarsvp" element={<MediaRSVP />} />
-
-            <Route path="/success" element={<Success />} />
-            <Route path="/success2" element={<Success2 />} />
+            <Route path="/Rose" element={<Rose/>} />
+            <Route path="/Said" element={<Said />} />
+            <Route path="/Farangez" element={<Farangez />} />
 
         </Routes>
       </BrowserRouter>
---
+++ src/components/Rose/index.js
@@ -0,0 +1,61 @@
+// import logo2 from '../Styles&Assets/logo2.png';
+import logo from '../Styles&Assets/logo.png';
+import whatstele from '../Styles&Assets/whatstele.png';
+import Email from '../Styles&Assets/Email.png';
+import Rose from '../Styles&Assets/Rose.png';
+import '../Styles&Assets/style.css';
+
+const VIPRSVP = () =>{
+
+    return(
+        <div  className='back' style={{display:"flex", flexDirection:"column", width:"100%", justifyContent:"center", alignItems:"center"}}>
+
+            <div className="mainer">
+
+                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginBottom: '10px'}}>
+                    <img style={{height: '180px', marginTop: '50px'}} src={Rose} alt="Employee Logo"/>
+                </div>
+
+                <div style={{display: 'flex', width: '100%', justifyContent: 'center', alignItems: 'center'}}> 
+                    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', maxWidth: '460px', marginTop: '10px'}}>
+                        <p style={{color: 'black', margin: '15px', fontSize: '20px', fontWeight: '900'}}>ROSE KITARIEVA</p>
+                        <p style={{color: 'black', fontSize: '16px', fontWeight: '300', fontStyle: 'italic', marginTop: '-15px'}}>Sales Agent</p>
+                        <p style={{color: 'black', margin: '15px', fontSize: '14px'}}>Westin Arabia Real Estate LLC ORN: 32133<br></br>Sheikh Zayed Road - Business Bay,<br></br>Burlington Tower, 23 Floor,<br></br>Office No. 2312 - 2316,<br></br>P.O. Box 118495, Dubai<br></br>United Arab Emirates | WestinArabia.com</p>
+                        
+                        <div style={{width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                            <div style={{width: '50%', display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end'}}>
+                                <img style={{height: '30px'}} src={whatstele} alt="Westin Logo"/>
+                            </div>
+
+                            <div style={{width: '50%', display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start'}}>
+                                <p style={{color: 'black', margin: '15px', fontSize: '14px'}}>054 252 7944<br></br>04 457 3296</p>
+                            </div>
+                        </div>
+
+
+                        <div style={{width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: '-20px'}}>
+                            <div style={{width: '40%', display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end'}}>
+                                <img style={{height: '30px'}} src={Email} alt="Westin Logo"/>
+                            </div>
+
+                            <div style={{width: '60%', display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start'}}>
+                                <p style={{color: 'black', margin: '15px', fontSize: '14px'}}>@westinarabia.com</p>
+                            </div>
+                        </div>
+
+                        
+                    </div>
+
+                </div>
+
+                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginBottom: '150px'}}>
+                    <img style={{width: '200px'}} src={logo} alt="Westin Logo"/>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default VIPRSVP
